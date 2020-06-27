ba362d38753b4a19ca463b51fd0344635dfefb61
RsXm <rsxm@users.noreply.github.com>
2020-06-27 10:56:28 +0200

    Update .gitignore and setup.py

--- setup.py
+++ setup.py
@@ -1,151 +1,45 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/en/latest/distributing.html
-"""
+from setuptools import setup
 
-from codecs import open
-from os import chdir, pardir, path
-
-from setuptools import find_packages, setup
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open('README.rst', 'r') as f:
     long_description = f.read()
-
-# allow setup.py to be run from any path
-chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
 
 setup(
     name='dstack-tasks',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    # use_scm_version={
-    #      'write_to': 'version.txt',
-    # },
-    # version=version_string,
     version='2.3.4',
-
     description=(
         "CLI that accompanies dstack-tasks make it easy to build and deploy application. "
         "Integrates with dstack-factory."),
-
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/obitec/dstack-tasks',
-
-    # Author details
-    author='JR Minnaar',
-    author_email='jrminnaar+pypi@gmail.com',
-
-    # Choose your license
+    author='J Minnaar',
+    author_email='jr.minnaar@gmail.com',
     license='MIT License',
-
-    # See https://pypi.python.org/pypi?:action=list_classifiers
+    keywords='docker python wheels images runtime automation deploy',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: System Administrators',
-
-        #  Topics
         'Topic :: Software Development :: Build Tools',
         'Topic :: System :: Installation/Setup',
         'Topic :: System :: Software Distribution',
-
-        # Environment
         'Operating System :: OS Independent',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-
-    # What does your project relate to?
-    keywords='docker python wheels images runtime automation deploy',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['docs', 'tests']),
-
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    # py_modules=["tasks"],
-
-    # If setuptools_scm is installed, this automatically adds everything in version control
+    packages=['dstack_tasks', ],
     include_package_data=True,
-
     zip_safe=True,
-
-    setup_requires=[
-        'wheel',
-        'setuptools_scm'
-    ],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'awscli',
         'boto3',
         'colorama',
-        'fabric>=2.0.0',
+        'fabric',
         'invoke',
         'python-dotenv',
         'requests',
-        'setuptools_scm',
     ],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': [
-            'twine'
-            'Sphinx>=1.4.1',
-            'wheel>=0.29.0',
-        ],
-    },
-
-    # test_suite='nose.collector',
-    # tests_require=['nose'],
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={
-    #     'sample': ['package_data.dat'],
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'dstack = dstack_tasks.main:program.run'
-        ],
-    },
+    extras_require={'dev': ['twine', 'wheel']},
+    entry_points={'console_scripts': ['dstack = dstack_tasks.main:program.run']},
 )
